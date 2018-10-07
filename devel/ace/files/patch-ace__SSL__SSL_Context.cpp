33f91d89f84741aa51aeda980a1bbc1e4a0fe7b5
Franco Fichtner <franco.fichtner@hardenedbsd.org>
2018-10-07 06:31:07 +0200

    HBSD: resolve merge conflict in devel/ace, fix LibreSSL build while at it
    Signed-off-by: Franco Fichtner <franco.fichtner@hardenedbsd.org>

--- devel/ace/files/patch-ace__SSL__SSL_Context.cpp
+++ devel/ace/files/patch-ace__SSL__SSL_Context.cpp
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
---- ace/SSL/SSL_Context.cpp.orig	2016-05-02 07:59:38 UTC
+--- ace/SSL/SSL_Context.cpp.orig	2018-09-18 07:26:56 UTC
 +++ ace/SSL/SSL_Context.cpp
-@@ -352,7 +352,7 @@ ACE_SSL_Context::filter_versions (const 
+@@ -356,7 +356,7 @@ ACE_SSL_Context::filter_versions (const 
  bool
  ACE_SSL_Context::check_host (const ACE_INET_Addr &host, SSL *peerssl)
  {
@@ -10,21 +9,3 @@
  
    this->check_context ();
  
-@@ -629,7 +629,7 @@ ACE_SSL_Context::random_seed (const char
- int
- ACE_SSL_Context::egd_file (const char * socket_file)
- {
--#if OPENSSL_VERSION_NUMBER < 0x00905100L
-=======
---- ace/SSL/SSL_Context.cpp.orig	2018-09-18 07:26:56 UTC
-+++ ace/SSL/SSL_Context.cpp
-@@ -179,7 +179,7 @@ ACE_SSL_Context::ssl_library_init (void)
- # endif  /* OPENSSL_VERSION_NUMBER < 0x10100000L */
- #endif  /* WIN32 */
- 
--#if OPENSSL_VERSION_NUMBER >= 0x00905100L
->>>>>>> freebsd/master
-+#if OPENSSL_VERSION_NUMBER < 0x00905100L || defined(LIBRESSL_VERSION_NUMBER)
-       // OpenSSL < 0.9.5 doesn't have EGD support.
- 
-       const char *egd_socket_file =
