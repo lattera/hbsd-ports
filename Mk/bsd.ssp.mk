adf45e5bd32167dbd5aeec8647dc512d4a90d4b7
Shawn Webb <shawn.webb@hardenedbsd.org>
2019-04-24 18:00:30 -0400

    HBSD: Resolve merge conflict
    Signed-off-by:	Shawn Webb <shawn.webb@hardenedbsd.org>

--- Mk/bsd.ssp.mk
+++ Mk/bsd.ssp.mk
@@ -4,15 +4,9 @@
 SSP_Include_MAINTAINER=	portmgr@FreeBSD.org
 
 .if !defined(SSP_UNSAFE) && \
-<<<<<<< HEAD
-    (${ARCH} == i386 || ${ARCH} == amd64)
+    (${MACHINE} != "mips")
 # Overridable as a user may want to use -fstack-protector
 SSP_CFLAGS?=	-fstack-protector-all
-=======
-    (${MACHINE} != "mips")
-# Overridable as a user may want to use -fstack-protector-all
-SSP_CFLAGS?=	-fstack-protector-strong
->>>>>>> upstream/master
 CFLAGS+=	${SSP_CFLAGS}
 LDFLAGS+=	${SSP_CFLAGS}
 .endif
